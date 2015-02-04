7a58a0df6c8122fbed5004cf375d6c1cec55d6f0
Jerome C <jcombes@sierrawireless.com>
2015-02-04 17:31:44 +0100

    Use global variable to configure tech-logger

--- techLogger.js
+++ techLogger.js
@@ -1,14 +1,11 @@
 /**
  * Our Logger that wrap Winston logging library.
- * It also synchronizes the various workers in multi cluster environnement.
- *
- * Only the master cluster is allowed to call Winston.
  */
-var _ = require("lodash");
-var cluster = require("cluster");
 var winston = require("winston");
 var splash = require("./lib/splash");
 
+var initialized = false;
+var defaulogger;
 var consologger;
 var filogger;
 var syslogger;
@@ -17,156 +14,80 @@ var syslogOptions;
 module.exports = {
 
     /**
-     * Setup winston
-     *
+     * Setup the logger configuration
+     * @param  {Object} config                           See below for details
+     * @param  {Object} config.syslog                    Syslog configuration (optional)
+     * @param  {Object} config.syslog.host               The host running syslogd.
+     * @param  {Object} config.syslog.appName            The name of the application.
+     * @param  {Object} config.syslog.thresholdLevel     Minimum level of messages that the logger should log. Default is info.
+     * @param  {Object} config.file                      Syslog configuration (optional)
+     * @param  {Object} config.file.name                 The filename of the logfile to write.
+     * @param  {Object} config.file.maxSize              Max size in bytes of the logfile, if the size is exceeded then a new file is created.
+     * @param  {Object} config.file.maxNumber            Limit the number of files created when the size of the logfile is exceeded.
+     * @param  {Object} config.file.json                 If true, messages will be logged as JSON. Default is false.
+     * @param  {Object} config.file.thresholdLevel       Minimum level of messages that the logger should log. Default is info.
      */
-    setup: function(configuration) {
-
-        // Logger configuration
-        // Winston levels correctly ordered
-        // DO NOT TRUST Winston level definition as it does not comply
-        // to a "logical" threshold mechanism
-        var levelsConfig = {
-            levels: {
-                emerg: 7,
-                alert: 6,
-                crit: 5,
-                error: 4,
-                warning: 3,
-                notice: 2,
-                info: 1,
-                debug: 0
-            },
-            colors: {
-                emerg: "magenta",
-                alert: "magenta",
-                crit: "red",
-                error: "red",
-                warning: "yellow",
-                notice: "green",
-                info: "blue",
-                debug: "cyan"
-            }
-        };
-
-        consologger = new(winston.Logger)({
-            transports: [
-                new winston.transports.Console({
-                    colorize: true,
-                    level: "debug"
-                })
-            ]
-        });
-        consologger.setLevels(levelsConfig.levels);
-
-        if (configuration.logging.file) {
-            filogger = new(winston.Logger)({
-                transports: [
-                    new winston.transports.File({
-                        filename: configuration.logging.file.name,
-                        maxsize: configuration.logging.file.maxSize,
-                        maxFiles: configuration.logging.file.maxNumber,
-                        json: configuration.logging.file.json,
-                        level: "info"
-                    })
-                ]
-            });
-            filogger.setLevels(levelsConfig.levels);
-        }
-
-        if (configuration.logging.syslog) {
-            syslogOptions = configuration.logging.syslog;
-            require("winston-syslog");
-            // syslog options
-            var options = {
-                host: configuration.logging.syslog.host,
-                port: 514,
-                protocol: "udp4",
-                app_name: configuration.logging.syslog.appName,
-                facility: "local0",
-                level: configuration.logging.syslog.thresholdLevel
-            };
-
-
-            syslogger = new(winston.Logger)({
-                colors: winston.config.syslog.colors,
-                transports: [
-                    new winston.transports.Syslog(options)
-                ]
-            });
-            syslogger.setLevels(levelsConfig.levels);
-        }
-
-        winston.addColors(levelsConfig.colors);
-    },
-
-    /**
-     * Register a new worker by listening to its messages of type "log"
-     *
-     * @param  {Worker} worker
-     *         The worker to listen
-     */
-    register: function(worker) {
-        worker.process.on("message", function(message) {
-            if (_.isObject(message) && message.type === "log") {
-                doLog(message);
-            }
-        });
+    setup: function(config) {
+        global.NODE_TECH_LOGGER_CFG = config;
     },
 
     debug: function() {
-        logOrForward("debug", Array.prototype.slice.call(arguments));
+        _log("debug", Array.prototype.slice.call(arguments));
     },
 
     info: function() {
-        logOrForward("info", Array.prototype.slice.call(arguments));
+        _log("info", Array.prototype.slice.call(arguments));
     },
 
     notice: function() {
-        logOrForward("notice", Array.prototype.slice.call(arguments));
+        _log("notice", Array.prototype.slice.call(arguments));
     },
 
     warn: function() {
-        logOrForward("warning", Array.prototype.slice.call(arguments));
+        _log("warning", Array.prototype.slice.call(arguments));
     },
 
     error: function() {
-        logOrForward("error", Array.prototype.slice.call(arguments));
+        _log("error", Array.prototype.slice.call(arguments));
     },
 
     crit: function() {
-        logOrForward("crit", Array.prototype.slice.call(arguments));
+        _log("crit", Array.prototype.slice.call(arguments));
     },
 
     alert: function() {
-        logOrForward("alert", Array.prototype.slice.call(arguments));
+        _log("alert", Array.prototype.slice.call(arguments));
     },
 
     emerg: function() {
-        logOrForward("emerg", Array.prototype.slice.call(arguments));
+        _log("emerg", Array.prototype.slice.call(arguments));
     },
 
     createExpressLoggerStream: function(level) {
         return {
             write: function(message /*, encoding */ ) {
-                logOrForward(level, message);
+                _log(level, message);
             }
         };
     },
+
     splash: function(app, configuration) {
         splash(this, app, configuration);
     }
 };
 
-
 /**
- * Log the message using winston
+ * Log the message using winston or console
  *
  * @param  {Object} log
  *             {String] log.level
  *             {String} log.message
  */
-function doLog(log) {
+function _doLog(log) {
+
+    if (defaulogger) {
+        defaulogger.log(log.level, log.message);
+    }
 
     if (consologger) {
         consologger.log(log.level, log.message);
@@ -182,18 +103,12 @@ function doLog(log) {
 }
 
 /**
- * Only the master process is allowed to log to avoid issues on the
- * file rolling mechanism.
+ * Log a message with the specified level
  *
- * Logging requests comming from the master are
- * directly logged through winston.
- * Those comming from a worker are forwarded to the master
- *
- * @param  {[type]} level [description]
- * @param  {Array} log   [list of message parts]
- * @return {[type]}       [description]
+ * @param  {String} level  the level
+ * @param  {Array}  log    list of message parts
  */
-function logOrForward(level, log) {
+function _log(level, log) {
 
     var message = log.map(function(element) {
         if (typeof element !== "string") {
@@ -203,19 +118,133 @@ function logOrForward(level, log) {
         }
     }).join(" ");
 
-    // We're in master we can directly log
-    if (cluster.isMaster) {
-        doLog({
-            level: level,
-            message: message
-        });
-        return;
+    if (!initialized) {
+        _initialize();
+        initialized = true;
     }
 
-    // Otherwise send a message to the master to let it log
-    process.send({
-        type: "log",
+    _doLog({
         level: level,
         message: message
     });
 }
+
+/**
+ * Initialize our logger
+ */
+function _initialize() {
+    var config = global.NODE_TECH_LOGGER_CFG;
+    if (config) {
+        _setupLogger(config);
+    } else {
+        if (!global.NODE_TECH_LOGGER_INIT) {
+            global.NODE_TECH_LOGGER_INIT = true;
+            var msg = "\n\n!!! =========================================== !!!";
+            msg += "\n!!!  No configuration set for node-tech-logger  !!!";
+            msg += "\n!!! =========================================== !!!\n\n";
+            console.warn(msg);
+        }
+        defaulogger = new _DefaultLogger();
+    }
+}
+
+/**
+ * Setup winston logger
+ */
+function _setupLogger(config) {
+
+    // Logger configuration
+    // Winston levels correctly ordered
+    // DO NOT TRUST Winston level definition as it does not comply
+    // to a "logical" threshold mechanism
+    var levelsConfig = {
+        levels: {
+            emerg: 7,
+            alert: 6,
+            crit: 5,
+            error: 4,
+            warning: 3,
+            notice: 2,
+            info: 1,
+            debug: 0
+        },
+        colors: {
+            emerg: "magenta",
+            alert: "magenta",
+            crit: "red",
+            error: "red",
+            warning: "yellow",
+            notice: "green",
+            info: "blue",
+            debug: "cyan"
+        }
+    };
+
+    consologger = new(winston.Logger)({
+        transports: [
+            new winston.transports.Console({
+                colorize: true,
+                level: "debug"
+            })
+        ]
+    });
+    consologger.setLevels(levelsConfig.levels);
+
+    if (config.file) {
+        filogger = new(winston.Logger)({
+            transports: [
+                new winston.transports.File({
+                    filename: config.file.name,
+                    maxsize: config.file.maxSize,
+                    maxFiles: config.file.maxNumber,
+                    json: config.file.json || false,
+                    level: config.file.thresholdLevel || "info"
+                })
+            ]
+        });
+        filogger.setLevels(levelsConfig.levels);
+    }
+
+    if (config.syslog) {
+        syslogOptions = config.syslog;
+        require("winston-syslog");
+        // syslog options
+        var options = {
+            host: config.syslog.host,
+            port: 514,
+            protocol: "udp4",
+            app_name: config.syslog.appName,
+            facility: "local0",
+            level: config.syslog.thresholdLevel || "info"
+        };
+
+        syslogger = new(winston.Logger)({
+            colors: winston.config.syslog.colors,
+            transports: [
+                new winston.transports.Syslog(options)
+            ]
+        });
+        syslogger.setLevels(levelsConfig.levels);
+    }
+
+    winston.addColors(levelsConfig.colors);
+}
+
+/**
+ * Default logger using console
+ */
+function _DefaultLogger() {
+    this.logFunctions = {
+        "debug": console.log,
+        "info": console.info,
+        "notice": console.info,
+        "warning": console.warn,
+        "error": console.error,
+        "crit": console.error,
+        "alert": console.error,
+        "emerg": console.error
+    };
+}
+_DefaultLogger.prototype.log = function(level, message) {
+    (this.logFunctions[level] || console.log)(message);
+};
